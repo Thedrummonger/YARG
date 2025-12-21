444c34792b3a9df10eab9622486ea23e152c4fd6
Thedrummonger <drummerxtmail@gmail.com>
2025-12-21 12:29:29 -0600

    Bare bones functionality, should be able to complete a seed from start to finish. Still needs ui for AP server connection, currently connects to local host as Player1.
    Working:
    -Check AP location on song completion
    -List available unplayed songs under their own header
    -Deathlink sending (no ability to turn it on yet)
    -Displaying AP messages in yarg through Toasts
    -Adding Star power upon recieving the YARG Gem item

---
+++ Assets/Script/YargAP/APEvents.cs
@@ -0,0 +1,114 @@
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
+using DG.Tweening.Core.Easing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using YARG.Gameplay;
+using YARG.Gameplay.Player;
+using YARG.Menu.Persistent;
+
+namespace YARG.Assets.Script.YargAP
+{
+    internal class APEvents
+    {
+        public static string GoalHash;
+        public static GameManager CurrentSong = null;
+        public static bool PrintChatMessages = true;
+        public static bool PrintUnrelatedItems = false;
+        public static ArchipelagoSession session;
+        public static DeathLinkService deathLinkService;
+        public static bool _isConnected => APEvents.session?.Socket != null && APEvents.session.Socket.Connected;
+
+        public static HashSet<string> RecievedSongs = new HashSet<string>();
+
+        public static void UpdateRecievedSongs()
+        {
+            if (!_isConnected)
+                return;
+
+            foreach(var item in session.Items.AllItemsReceived)
+            {
+                if (APData.APItemIDToHash().TryGetValue(item.ItemId, out var data))
+                    RecievedSongs.Add(data);
+            }
+        }
+
+        public static HashSet<string> GetUnplayedAvailableLocations()
+        {
+            HashSet<string> AvailableSongLocations = new();
+            if (!_isConnected)
+                return AvailableSongLocations;
+            foreach (var i in RecievedSongs)
+            {
+                if (APData.SongHashToAPLocations().TryGetValue(i, out var locations) &&  locations.Any(x => !session.Locations.AllLocationsChecked.Contains(x)))
+                    AvailableSongLocations.Add(i);
+            }
+            return AvailableSongLocations;
+        }
+
+        public static void Items_ItemReceived(Archipelago.MultiClient.Net.Helpers.ReceivedItemsHelper helper)
+        {
+            UpdateRecievedSongs();
+            while (helper.Any())
+            {
+                var item = helper.DequeueItem();
+
+                //Item ID 1 is Yarg Gem, for now I just made it grant star power
+                if (item.ItemId == (long)APData.APFiller.YargGem && CurrentSong != null)
+                    foreach (var i in CurrentSong.Players)
+                        ApplyStarPowerItem(i, CurrentSong);
+
+            }
+        }
+
+        public static void MessageLog_OnMessageReceived(Archipelago.MultiClient.Net.MessageLog.Messages.LogMessage message)
+        {
+            if (message is Archipelago.MultiClient.Net.MessageLog.Messages.ChatLogMessage chatMessage && !PrintChatMessages)
+                return;
+            if (message is Archipelago.MultiClient.Net.MessageLog.Messages.ItemSendLogMessage itemMessage && !itemMessage.IsReceiverTheActivePlayer && !PrintUnrelatedItems)
+                return;
+            ToastManager.ToastMessage(message.ToString());
+        }
+
+        private static MethodInfo _gainStarPower;
+        public static void ApplyStarPowerItem(BasePlayer player, GameManager handler)
+        {
+            if (handler == null)
+                return;
+
+            Debug.Log("Processing Star Power Item");
+
+            var engine = player.BaseEngine;
+            if (engine == null) return;
+
+            //Since we can;t edit yarg core, we have to use reflection to call GainStarPower since it is private
+            _gainStarPower ??= engine.GetType().GetMethod("GainStarPower", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            if (_gainStarPower == null)
+            {
+                Debug.LogError("Failed to access GainStarPower with reflection");
+                return;
+            }
+            _gainStarPower.Invoke(engine, new object[] { engine.TicksPerQuarterSpBar });
+        }
+
+        internal static void TryCheckSongLocation(GameManager gameManager)
+        {
+            if (!_isConnected || !RecievedSongs.Contains(gameManager.Song.Hash.ToString()))
+                return;
+
+            if (APData.SongHashToAPLocations().TryGetValue(gameManager.Song.Hash.ToString(), out var Locations))
+                session.Locations.CompleteLocationChecksAsync(Locations);
+
+            if (GoalHash != null && GoalHash == gameManager.Song.Hash.ToString())
+                session.SetGoalAchieved();
+
+        }
+    }
+}
---
+++ Assets/Script/YargAP/ArchipelagoManager.cs
@@ -0,0 +1,66 @@
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+using YARG.Gameplay;
+using YARG.Gameplay.Player;
+using YARG.Menu.Persistent;
+using static UnityEngine.Rendering.STP;
+
+namespace YARG.Assets.Script.YargAP
+{
+    internal class ArchipelagoManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            // I have attached this class to the persistant scene under integration so it runs when Yarg loads
+            // For now it just connects to a local archipelago server with hardcoded credentials, but later the connection can
+            // be made through the menu. This will probably be repurposed to just run any initialization code we need.
+            ConnectTestSession();
+        }
+        public void ConnectTestSession()
+        {
+            APEvents.session = ArchipelagoSessionFactory.CreateSession("localhost");
+            var Result = APEvents.session.TryConnectAndLogin("YARG", "Player1", Archipelago.MultiClient.Net.Enums.ItemsHandlingFlags.AllItems);
+            if (Result is LoginFailure failure)
+            {
+                Debug.LogError("Failed to connect to Archipelago server: " + string.Join(Environment.NewLine, failure.Errors));
+                APEvents.session = null;
+                return;
+            }
+            Debug.Log("Connected to Archipelago server successfully!");
+
+            APEvents.session.MessageLog.OnMessageReceived += APEvents.MessageLog_OnMessageReceived;
+            APEvents.session.Items.ItemReceived += APEvents.Items_ItemReceived;
+
+            var SlotData = APEvents.session.DataStorage.GetSlotData();
+            if (SlotData["Goal Song"] is string GoalSongName && APData.SongHashMap.Values.Any(x => x == GoalSongName))
+            {
+                APEvents.GoalHash = APData.SongHashMap.First(x => x.Value == GoalSongName).Key;
+            }
+            else
+            {
+                ToastManager.ToastError($"Could not get Goal Song. Report this to the APworld Devs!");
+                Debug.LogError($"Could not get Goal Song {JsonConvert.SerializeObject(SlotData)}");
+            }
+
+            bool DeathLinkEnabled = false;
+            //We can use the yaml to enable death link and grab the option from slot data.
+            if (DeathLinkEnabled)
+            {
+                APEvents.deathLinkService = DeathLinkProvider.CreateDeathLinkService(APEvents.session);
+                APEvents.deathLinkService.EnableDeathLink();
+            }
+
+            APEvents.UpdateRecievedSongs();
+        }
+    }
+}
