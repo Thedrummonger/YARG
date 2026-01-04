4f6250f7584a77e4f9c467c5e2c895b370bc7622
Thedrummonger <drummerxtmail@gmail.com>
2026-01-03 20:33:52 -0600

    It works somehow

--- Assets/Script/Menu/Archipelago/ArchipelagoMenu.cs
+++ Assets/Script/Menu/Archipelago/ArchipelagoMenu.cs
@@ -1,13 +1,74 @@
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
+using Mono.Unix.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using YARG.Assets.Script.YargAP;
 using YARG.Core.Input;
+using YARG.Menu.History;
+using YARG.Menu.ListMenu;
+using YARG.Menu.Navigation;
+using YARG.Menu.Persistent;
+using YARG.Menu.Settings;
+using YARG.Settings;
+using YARG.Settings.Customization;
+using YARG.Song;
 
-namespace YARG.Menu.Archipelago
+namespace YARG.Menu.ArchipelagoMenu
 {
-    public class ArchipelagoMenu : MonoBehaviour
+    public class ArchipelagoMenu : MonoSingleton<ArchipelagoMenu>
     {
-        
+        public TMP_InputField ServerAddress;
+        public TMP_InputField Port;
+        public TMP_InputField Slotname;
+        public TMP_InputField Password;
+
+        public Toggle PrintChat;
+        public Toggle PrintUnrelatedItems;
+
+        public TMP_Dropdown DeathLinkMode;
+
+        private void OnEnable()
+        {
+            // Set navigation scheme
+            Navigator.Instance.PushScheme(new NavigationScheme(new()
+            {
+                new NavigationScheme.Entry(MenuAction.Red, "Menu.Common.Back", () => MenuManager.Instance.PopMenu())
+
+            }, true));
+
+        }
+
+        public void ConnectButton() => APConnectionHelper.DoConnect($"{ServerAddress.text}:{Port.text}", Slotname.text, Password.text);
+        public void DisconnectButton() => APConnectionHelper.DoDisconnect();
+
+        public void SetToggles()
+        {
+            APEvents.PrintChatMessages = PrintChat.isOn;
+            APEvents.PrintUnrelatedItems = PrintUnrelatedItems.isOn;
+            APEvents.deathLinkOverride = DeathLinkMode.value;
+
+            Debug.Log($"{APEvents.PrintChatMessages} | {APEvents.PrintUnrelatedItems} | {APEvents.deathLinkOverride}");
+        }
+
+        private void OnDisable()
+        {
+            Navigator.Instance.PopScheme();
+
+            // Save on close
+            SettingsManager.SaveSettings();
+            CustomContentManager.SaveAll();
+
+            //This is a bit of a hack to update the CurrentNavigationGroup again.
+            //ideally the settings menu should work just like every other menu so this isn't needed
+            MenuManager.Instance.ReactivateCurrentMenu();
+        }
     }
 }
---
+++ Assets/Script/YargAP/APConnectionHelper.cs
@@ -0,0 +1,125 @@
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
+using Mono.Unix.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using YARG.Menu.Persistent;
+using YARG.Song;
+
+namespace YARG.Assets.Script.YargAP
+{
+    internal class APConnectionHelper
+    {
+        public static void DoConnect(string ServerAddress, string slotName, string Password)
+        {
+            if (APEvents._isConnected) return;
+            APEvents.session = ArchipelagoSessionFactory.CreateSession(ServerAddress);
+            var Result = APEvents.session.TryConnectAndLogin("YARG", slotName, Archipelago.MultiClient.Net.Enums.ItemsHandlingFlags.AllItems, password: Password);
+            if (Result is LoginFailure failure)
+            {
+                ToastManager.ToastError("Failed to connect to Archipelago server: " + string.Join(Environment.NewLine, failure.Errors));
+                APEvents.session = null;
+                return;
+            }
+
+            var SlotData = APEvents.session.DataStorage.GetSlotData();
+
+            if (SlotData.ContainsKey("songlist"))
+            {
+                var songlistObj = (JObject) SlotData["songlist"];
+                APData.SongNames = songlistObj.ToObject<Dictionary<string, int[]>>();
+                APData.NeedsRegen = true;
+            }
+            else
+            {
+                ToastManager.ToastError($"Unable to parse song list. Report this to the APworld Devs!");
+                Debug.LogError($"Unable to parse song list {JsonConvert.SerializeObject(SlotData)}");
+                APEvents.session.Socket.DisconnectAsync();
+                APEvents.session = null;
+                return;
+            }
+
+            bool WasMissingSong = false;
+            foreach (var i in APData.SongNames)
+                if (!SongContainer.Songs.Any(x => x.Name == i.Key))
+                {
+                    WasMissingSong = true;
+                    Debug.LogError($"{i.Key} Was not found in the current yarg song list");
+                }
+
+            if (WasMissingSong)
+                DialogManager.Instance.ShowMessage("Missing Song Error", "One or more songs were not found in your YARG setlist\nEnsure you are using the YARG official setlist!");
+
+
+            if (SlotData["Goal Song"] is string GoalSongName && APData.SongNames.ContainsKey(GoalSongName))
+            {
+                Debug.Log($"Goal Song {GoalSongName}");
+                APEvents.GoalSong = GoalSongName;
+            }
+            else
+            {
+                ToastManager.ToastError($"Could not get Goal Song. Report this to the APworld Devs!");
+                Debug.LogError($"Could not get Goal Song {JsonConvert.SerializeObject(SlotData)}");
+                APEvents.session.Socket.DisconnectAsync();
+                APEvents.session = null;
+                return;
+            }
+
+            if (SlotData["Gems Required"] is long GoalItemsNeeded)
+            {
+                Debug.Log($"Gems Needed {GoalItemsNeeded}");
+                APEvents.GoalItemNeeded = (int) GoalItemsNeeded;
+            }
+            else
+            {
+                ToastManager.ToastError($"Could not get Goal Item Requirement. Report this to the APworld Devs!");
+                Debug.LogError($"Could not get Goal Song {JsonConvert.SerializeObject(SlotData)}");
+                APEvents.session.Socket.DisconnectAsync();
+                APEvents.session = null;
+                return;
+            }
+
+            if (SlotData.TryGetValue("Goal Song Visibility", out var GSV) && GSV is Int64 VI)
+            {
+                APEvents.goalDisplaySetting = (APData.GoalDisplaySetting) VI;
+            }
+
+            APEvents.session.MessageLog.OnMessageReceived += APEvents.MessageLog_OnMessageReceived;
+            APEvents.session.Items.ItemReceived += APEvents.Items_ItemReceived;
+
+            if (SlotData.TryGetValue("Death Link", out var DLO) && DLO is long DLI && DLI > 0)
+            {
+                APEvents.deathLinkService = DeathLinkProvider.CreateDeathLinkService(APEvents.session);
+                APEvents.deathLinkService.EnableDeathLink();
+                APEvents.deathLinkService.OnDeathLinkReceived += APEvents.ProcessDeathLink;
+                APEvents.deathLinkType = DLI > 1 ? APData.DeathLinkType.Fail : APData.DeathLinkType.RockMeter;
+
+            }
+
+            ToastManager.ToastInformation("Connected to Archipelago server successfully!");
+
+            APEvents.UpdateRecievedSongs();
+        }
+
+        public static void DoDisconnect()
+        {
+            if (APEvents._isConnected)
+                APEvents.session.Socket.DisconnectAsync();
+
+            APEvents.session.MessageLog.OnMessageReceived -= APEvents.MessageLog_OnMessageReceived;
+            APEvents.session.Items.ItemReceived -= APEvents.Items_ItemReceived;
+            APEvents.GoalSong = null;
+            APEvents.deathLinkService = null;
+            APData.SongNames = new Dictionary<string, int[]>();
+            APData.NeedsRegen = true;
+            ToastManager.ToastInformation("Disconnected from Archipelago!");
+        }
+    }
+}
