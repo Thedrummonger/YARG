f8d6bfe1c0abbde36155b216e056e6428cdc8515
energymaster22 <energymastervr@gmail.com>
2026-03-16 20:26:28 -0400

    Commented out unused usings that were causing build errors

--- Assets/Script/Yarchipelago/Menu.Connection.cs
+++ Assets/Script/Yarchipelago/Menu.Connection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 using UnityEngine;
 using YARG.Menu.Persistent;
 
